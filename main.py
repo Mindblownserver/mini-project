a8c671589a8e4b62cee8dfdefaeebe4662ec5b51
yassine <med.yassine.kharrat@gmail.com>
2023-04-01 18:19:55 +0100

    added icons to project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import os
-
+from Personne.Afficher.afficherTout import AfficherWindow
 # Initialisation de l'application
 
 
@@ -11,8 +11,17 @@ class MainWindow(QMainWindow):
         path = os.path.dirname(__file__) + "/"
         ui_file = path+"GUI.ui"
         loadUi(ui_file, self)
+        
+        
+        self.actionAfficher.triggered.connect(self.openAfficher)
+         
         self.show()
-
+    def clicked(self, message):
+        print(message)
+    def openAfficher(self):
+        self.windpw = QMainWindow()
+        self.uiAfficher = AfficherWindow()
+        self.uiAfficher.show()
 
 if __name__ == '__main__':
     app = QApplication([])
--- test/home.py
+++ test/home.py
@@ -2,41 +2,25 @@ from PyQt5 import QtWidgets, uic
 import os
 class Page1(QtWidgets.QWidget):
     def __init__(self, parent=None):
-        super().__init__(parent)
+        QtWidgets.QWidget.__init__(self)
         path = os.path.dirname(__file__) + "/"
         uic.loadUi(path+'first.ui', self)
 
 class Page2(QtWidgets.QWidget):
     def __init__(self, parent=None):
-        super().__init__(parent)
+        QtWidgets.QWidget.__init__(self)
         path = os.path.dirname(__file__) + "/"
         uic.loadUi(path+'second.ui', self)
 
 class App(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Multi-Page App')
+        def __init__(self):
+            QtWidgets.QMainWindow.__init__(self)
 
-        # Create a stacked widget to manage pages
-        self.stacked_widget = QtWidgets.QStackedWidget(self)
-        self.setCentralWidget(self.stacked_widget)
+            wid = Page1()
 
-        # Add pages to the stacked widget
-        self.page1 = Page1()
-        self.stacked_widget.addWidget(self.page1)
-
-        self.page2 = Page2()
-        self.stacked_widget.addWidget(self.page2)
-
-        # Connect signals and slots to switch between pages
-        self.page1.pushButton.clicked.connect(self.switch_to_page2)
-        self.page2.pushButton.clicked.connect(self.switch_to_page1)
-
-    def switch_to_page1(self):
-        self.stacked_widget.setCurrentWidget(self.page1)
-
-    def switch_to_page2(self):
-        self.stacked_widget.setCurrentWidget(self.page2)
+            self.grid = QtWidgets.QGridLayout()
+            self.grid.addWidget(wid,0,0)
+            self.frame.setLayout(self.grid)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
