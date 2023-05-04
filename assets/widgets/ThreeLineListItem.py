c828710c5cba50576ac71129e55e9a394245519d
yassine <med.yassine.kharrat@gmail.com>
2023-05-04 20:24:10 +0100

    added custom widget

---
+++ assets/widgets/ThreeLineListItem.py
@@ -0,0 +1,16 @@
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QWidget
+from PyQt5.uic import loadUi
+import os
+class ListItem(QWidget):
+    def __init__(self,nom,prenom,age,cin):
+        super().__init__()
+        path = os.path.dirname(__file__) + "/"
+        ui_file = path+"ThreeLineListItem.ui"
+        loadUi(ui_file,self)
+        # load Data
+        self.nom.setText(f"{prenom} {nom}")
+        self.cin.setText(cin)
+        self.age.setText(f"{age} ans")
+        self.show()
